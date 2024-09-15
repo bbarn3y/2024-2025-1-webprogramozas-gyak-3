aaebb208ee340ad9be13c4085f8044d3aef0b2b2
barnabas.balazs <barnabas.balazs@enliven.systems>
2024-09-15 09:38:16 +0200

    2. gyakorlat starter kit

---
+++ gyak2/array/array.js
@@ -0,0 +1,75 @@
+const students = [
+    {
+        name: "Dudás Levente János",
+        age: 30,
+        hobbies: ["Futás", "Hangya égetés", "Hegymászás"],
+    },
+    {
+        name: "Fekete Gyöngy",
+        age: 24,
+        hobbies: ["Műanyag villák gyűjtése különböző büfékből", "Úszás"],
+    },
+    {
+        name: "András Dávid",
+        age: 41,
+        hobbies: ["Biciklizés", "LEGO", "Villanykapcsolók fel-le kapcsolgatása"],
+    },
+    {
+        name: "Bollók Ármin",
+        age: 18,
+        hobbies: ["LEGO", "Rajzolás"],
+    },
+    {
+        name: "Bálint Martin Attila",
+        age: 21,
+        hobbies: ["Ételek betűrendbe rendezése a kamrában"],
+    },
+    {
+        name: "Faragó Flóra",
+        age: 21,
+        hobbies: ["Biciklizés", "Falmászás", "Golf", "Kavicshajítás", "LEGO", "Motorozás"],
+    },
+];
+
+// 22 év feletti személyek
+const atLeast22YearsOld = undefined;
+
+// Az egyetlen 20 év alatti személy
+const under20 = undefined;
+
+// Mindenki 40 év alatti?
+const everyoneUnder40 = undefined;
+
+// Legöregebb személy
+const oldestPerson = undefined;
+
+// Az első bicikliző indexe a tömbben
+const firstCyclerIndex = undefined;
+
+// ABC-rendben a 3. személy
+const thirdPerson = undefined;
+
+// Szeretnek LEGOzni
+const likesLEGO = undefined;
+
+// Hobbik átlagos száma
+const averageHobbyCount = undefined;
+
+// Hobbik egyedi listája
+const uniqueHobbies = undefined;
+
+// Egyedi keresztnevek
+const uniqueNames = undefined;
+
+console.log(
+    `Legalább 22 évesek:`,  atLeast22YearsOld,
+    '\nAz egyetlen 20 év alatti személy:', under20,
+    '\nMindenki 40 év alatti?', everyoneUnder40,
+    '\nLegöregebb személy:', oldestPerson,
+    '\nAz első bicikliző indexe a tömbben:', firstCyclerIndex,
+    '\nABC-rendben a 3. személy:', thirdPerson,
+    '\nSzeretnek LEGOzni: ', likesLEGO,
+    '\nHobbik átlagos száma:', averageHobbyCount,
+    '\nHobbik egyedi listája:', uniqueHobbies,
+    '\nUnique names', uniqueNames,
+);
---
+++ gyak2/dom-sqrt/sqrt.js
@@ -0,0 +1,9 @@
+// 1. Tervezés
+// 2. DOM
+// 3. Számolás / segédfüggvény
+// 4. Eseménykezelés
+// 5. Validáció
+// 6. Stílus
+
+
+
