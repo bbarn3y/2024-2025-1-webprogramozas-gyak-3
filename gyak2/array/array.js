539e04e552556b591579317f1c5996c2120e0406
barnabas.balazs <barnabas.balazs@enliven.systems>
2024-09-16 12:48:05 +0200

    2. gyakorlat WIP

--- gyak2/array/array.js
+++ gyak2/array/array.js
@@ -31,35 +31,51 @@ const students = [
     },
 ];
 
-// 22 év feletti személyek
-const atLeast22YearsOld = undefined;
+// 22 vagy több év feletti személyek
+const atLeast22YearsOld = students.filter(x => x.age >= 22);
 
 // Az egyetlen 20 év alatti személy
-const under20 = undefined;
+const under20 = students.find(s => s.age < 18);
 
 // Mindenki 40 év alatti?
-const everyoneUnder40 = undefined;
+const everyoneUnder40 = students.every(e => e.age < 40);
 
 // Legöregebb személy
-const oldestPerson = undefined;
+// const oldestPerson = students
+//     .sort((x, y) => x.age - y.age)[students.length -1];
+// const oldestPerson = students
+//     .reduce((partialMax, s) => partialMax.age > s.age ? partialMax : s,
+//         students[0])
+
+// Math.max(10, 23, 30,... )
+const maxAge = Math.max(...students.map(s => s.age));
+const oldestPerson = students.find(s => s.age === maxAge);
 
 // Az első bicikliző indexe a tömbben
-const firstCyclerIndex = undefined;
+const firstCyclerIndex = students
+    .findIndex(s => s.hobbies.includes('Biciklizés'));
 
 // ABC-rendben a 3. személy
-const thirdPerson = undefined;
+const thirdPerson = students.sort((s1, s2) =>
+    s1.name.localeCompare(s2.name))[2];
 
 // Szeretnek LEGOzni
-const likesLEGO = undefined;
+const likesLEGO = students
+    .filter(x => x.hobbies.includes('LEGO'));
 
 // Hobbik átlagos száma
-const averageHobbyCount = undefined;
+const averageHobbyCount = students.reduce((accumulated, s) =>
+    accumulated + s.hobbies.length, 0) / students.length;
 
 // Hobbik egyedi listája
-const uniqueHobbies = undefined;
+const uniqueHobbies = // students.map(x => x.hobbies).flat();
+    [
+        ...new Set(students.flatMap(x => x.hobbies))
+    ]
 
 // Egyedi keresztnevek
-const uniqueNames = undefined;
+const uniqueNames = students
+    .flatMap(x => x.name.split(' ').slice(1));
 
 console.log(
     `Legalább 22 évesek:`,  atLeast22YearsOld,
