b8e46feb3a6c322f0ad656c3d79c3b10943254e7
barnabas.balazs <barnabas.balazs@enliven.systems>
2024-09-16 13:14:12 +0200

    2. gyakorlat WIP

--- gyak2/dom-sqrt/sqrt.js
+++ gyak2/dom-sqrt/sqrt.js
@@ -1,8 +1,30 @@
 // 1. Tervezés
 // 2. DOM
 // 3. Számolás / segédfüggvény
+function calculateSQRT(a) {
+    return Math.sqrt(a);
+}
+
 // 4. Eseménykezelés
-// 5. Validáció
+const buttonEl = document.querySelector('#submitButton')
+const inputEl = document.querySelector('#numberInput')
+const resultEl = document.querySelector('#result')
+
+buttonEl.addEventListener('click', (event) => {
+    const value = +inputEl.value;
+
+    // 5. Validáció
+    if (!isNaN(value)) {
+        // resultEl.innerText = '<p>' + calculateSQRT(value) + '</p>';
+        resultEl.innerHTML =
+            `<p>${calculateSQRT(value)}</p>`;
+        inputEl.style.border = '';
+    } else {
+        inputEl.value = '';
+        inputEl.style.border = '1px solid red';
+    }
+})
+
 // 6. Stílus
 
 
