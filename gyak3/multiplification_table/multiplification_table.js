86a8227721788d98bc06dae8ef238891a5aafa3e
barnabas.balazs <barnabas.balazs@enliven.systems>
2024-09-23 12:50:58 +0200

    3. gyakorlat WIP

--- gyak3/multiplification_table/multiplification_table.js
+++ gyak3/multiplification_table/multiplification_table.js
@@ -12,3 +12,45 @@ function delegate(parent, type, selector, handler) {
 const inputEl = document.querySelector('#rows');
 const tableEl = document.querySelector('#multiTable');
 
+function maximizeSize(inputNumber) {
+    const maximizedNumber = Math.min(200, inputNumber);
+    inputEl.value = maximizedNumber
+    return maximizedNumber
+}
+
+function selectCell(cellEl) {
+    if (cellEl.classList.contains('selected')) {
+        cellEl.classList.remove('selected')
+    } else {
+        cellEl.classList.add('selected')
+    }
+}
+
+inputEl.addEventListener('input', (event) => {
+    const size = maximizeSize(+event.target.value);
+    console.log('size', size)
+
+    tableEl.innerHTML = '';
+    for (let i = 1; i <= size; i++) {
+        const rowEl = document.createElement('tr');
+        for (let j = 1; j <= size; j++) {
+            const cellEl = document.createElement('td');
+            cellEl.innerText = i*j;
+            cellEl.classList.add('cell')
+            cellEl.dataset['x'] = j;
+            cellEl.dataset['y'] = i;
+
+            cellEl.addEventListener('click', (e) => {
+                selectCell(cellEl);
+            })
+
+            rowEl.append(cellEl)
+        }
+        tableEl.append(rowEl);
+    }
+})
+
+
+
+
+
--- gyak3/propagation/propagation.js
+++ gyak3/propagation/propagation.js
@@ -1,2 +1,20 @@
-const outerRectangleEl = document.getElementById('outerRectangle');
-const innerRectangleEl = document.getElementById('innerRectangle');
+const outerRectangleEl =
+    document.getElementById('outerRectangle');
+const innerRectangleEl =
+    document.getElementById('innerRectangle');
+
+document.addEventListener('click', (e) => {
+    console.log('document', e, this);
+})
+
+outerRectangleEl.addEventListener('click', (e) => {
+    console.log('outerRectangleEl', e, this);
+    // e.stopPropagation();
+})
+
+innerRectangleEl.addEventListener('click', function (e) {
+    console.log('innerRectangleEl', e, this);
+})
+
+
+
