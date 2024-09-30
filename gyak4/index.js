c05634d2711e43b3017d67ae00d4f2bd9353c34f
barnabas.balazs <barnabas.balazs@enliven.systems>
2024-09-30 13:34:22 +0200

    Gyak 4

--- gyak4/index.js
+++ gyak4/index.js
@@ -15,3 +15,51 @@ const btnRestart = document.querySelector("#restart");
 
 // ========= Solution =========
 
+// a
+const word = wordList[random(0, wordList.length -1)];
+divTheWord.innerHTML = word;
+
+// b
+form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      spanError.innerHTML = '';
+
+      // c
+      if (inputGuess.value.length !== 5) {
+        spanError.innerHTML = 'The length of the word is not 5!'
+        return;
+      }
+
+      // d
+      if (!wordList.includes(inputGuess.value)) {
+        spanError.innerHTML = 'The word is not considered acceptable!'
+        return;
+      }
+
+      // e
+      const matches = inputGuess.value.split('')
+          .reduce((subMatches, char, index) =>
+            subMatches + (char === word[index] ? 1 : 0),
+            0
+          )
+      console.log('matches', matches);
+
+      // f
+      tableGuesses.innerHTML = `<tr ${matches === 5 ? 'class="correct"' : ""}>
+    <td>${inputGuess.value}</td>
+    <td>${matches}</td>
+</tr>
+` + tableGuesses.innerHTML;
+
+    if (matches === 5) {
+      divEndOfGame.removeAttribute('hidden');
+    }
+    }
+)
+
+
+
+
+
+
+
