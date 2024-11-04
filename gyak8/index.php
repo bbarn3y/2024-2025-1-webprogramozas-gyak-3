b59c7e562de7c7967c9e20829c790f9d8d639fd0
barnabas.balazs <barnabas.balazs@enliven.systems>
2024-11-04 13:00:48 +0100

    Gyak 8 WIP

--- gyak8/index.php
+++ gyak8/index.php
@@ -5,8 +5,8 @@ $formState = [
         'name' => 'Select one',
         'type' => 'radio',
         'options' => [
-            'First option' => true,
-            'Second option' => false
+            'First option' => false,
+            'Second option' => true
         ]
     ],
     2 => [
@@ -33,7 +33,23 @@ $formState = [
 
 <?php foreach ($formState as $id => $settings): ?>
 
+    <h3><?= $settings['name'] ?></h3>
 
+    <?php foreach ($settings['options'] as $option => $checked): ?>
+        <input type="<?= $settings['type'] ?>"
+               name="<?= $settings['name'] ?>>"
+               value="<?= $option ?>"
+               <?= $checked ? 'checked' : '' ?>
+        /> <?= $option ?>
+    <?php endforeach; ?>
 
 <?php endforeach; ?>
+
+
+<h3>SQRT</h3>
+<form method="POST" action="gyak8/squareRoot.php">
+    <label for="number">Number</label>
+    <input id="number" name="number" type="text" />
+    <button type="submit">Calculate SQRT</button>
+</form>
 
--- gyak8/squareRoot.php
+++ gyak8/squareRoot.php
@@ -1,6 +1,25 @@
 <?php
 
+// print_r($_GET);
+
+$number = $_GET['number'] ?? $_POST['number'] ?? NULL;
+
 $errors = [];
 
+if (!$number) {
+    $errors[] = 'Please provide a number!';
+}
 
+?>
+
+Number: <?= $number ?>
+<br>
+<?php if (count($errors) == 0): ?>
+    SQRT: <?= sqrt($number) ?>
+<?php endif; ?>
+
+<?php foreach ($errors as $error) {
+    echo '<div style="color: red">' . $error . '</div>';
+}
+?>
 
