ba6a779b9a453437fad2dfe6bd0ee2c2471ec34b
barnabas.balazs <barnabas.balazs@enliven.systems>
2024-11-23 08:14:37 +0100

    Gyak 11 starter kit

---
+++ gyak11/lobby.php
@@ -0,0 +1,10 @@
+Authenticated as:
+
+<button>User button</button>
+
+<button>Admin button</button>
+
+<form action="" method="post">
+    <input name="logout" hidden />
+    <button type="submit">Logout</button>
+</form>
---
+++ gyak11/login.php
@@ -0,0 +1,13 @@
+<form action="authenticator.php" method="post">
+    <div>
+        <label for="username">Username: </label><br>
+        <input type="text" name="username" id="username" value="<?= $_POST['username'] ?? "" ?>">
+    </div>
+    <div>
+        <label for="password">Password: </label><br>
+        <input type="password" name="password" id="password" value="password">
+    </div>
+    <div>
+        <button type="submit">Login</button>
+    </div>
+</form>
